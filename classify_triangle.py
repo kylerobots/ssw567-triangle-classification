ad7f97e7248b1add8b4ca3dd686e4fbea8e04cfa
Kyle M. Hart <kylemhart@gmail.com>
2021-09-10 11:32:44 -0400

    Consolidate cli call into same file as function.

--- classify_triangle.py
+++ classify_triangle.py
@@ -1,3 +1,6 @@
+import argparse
+
+
 def classify_triangle(s1: float, s2: float, s3: float) -> str:
     """
     @brief Classify the type of a triangle based on its sides.
@@ -48,3 +51,18 @@ def classify_triangle(s1: float, s2: float, s3: float) -> str:
         # If it isn't any of the above, it has to be scalene
         result += 'scalene'
     return result
+
+
+if __name__ == '__main__':
+    # Use of argparse comes from its documentation: https://docs.python.org/3/library/argparse.html
+    parser = argparse.ArgumentParser(
+        description='Classify a triangle based on side length.')
+    parser.add_argument('side_1', metavar='side_1',
+                        type=float, help='The first side of the triangle.')
+    parser.add_argument('side_2', metavar='side_2',
+                        type=float, help='The second side of the triangle.')
+    parser.add_argument('side_3', metavar='side_3',
+                        type=float, help='The third side of the triangle.')
+    args = parser.parse_args()
+    result = classify_triangle(args.side_1, args.side_2, args.side_3)
+    print('This is a {0:s} triangle.'.format(result))
