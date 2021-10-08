5e9ab4b3f1b0faef5082d69c8b3bb61e8748b8b8
Kyle M. Hart <kylemhart@gmail.com>
2021-10-08 10:29:47 -0400

    Clean up formatting in triangle module

--- classify_triangle.py
+++ classify_triangle.py
@@ -1,7 +1,10 @@
+"""
+A module that helps classify types of triangles.
+"""
 import argparse
 
 
-def classify_triangle(s1: float, s2: float, s3: float) -> str:
+def classify_triangle(s_1: float, s_2: float, s_3: float) -> str:
     """
     @brief Classify the type of a triangle based on its sides.
 
@@ -9,7 +12,8 @@ def classify_triangle(s1: float, s2: float, s3: float) -> str:
     * "equilateral": All three sides are the same length
     * "isosceles": Two sides are the same length
     * "scalene": All three sides are different lengths
-    * "right": The sides follow the Pythagorean theorem (this can be in addition to the previous three)
+    * "right": The sides follow the Pythagorean theorem (this can be in
+    addition to the previous three)
     * "invalid": The provided arguments do not represent a valid triangle
 
     @example
@@ -22,36 +26,39 @@ def classify_triangle(s1: float, s2: float, s3: float) -> str:
     @param s3 The last edge of the triangle (order does not matter)
     @return A string containing the classification of the triangle
     """
-    # First, verify that all arguments are numbers greater than 0. This can't be a triangle otherwise.
-    # This is also where any non-numeric types will throw errors. So watch for that and return "invalid" if it occurs.
+    # First, verify that all arguments are numbers greater than 0. This can't
+    # be a triangle otherwise.
+    # This is also where any non-numeric types will throw errors. So watch for
+    # that and return "invalid" if it occurs.
     try:
-        if not (s1 > 0.0 and s2 > 0.0 and s3 > 0.0):
+        if not (s_1 > 0.0 and s_2 > 0.0 and s_3 > 0.0):
             return 'invalid'
     except TypeError:
         return 'invalid'
-    # Verify the triangle inequality holds for all combinations of sides. Use the strict case to avoid degenerate
+    # Verify the triangle inequality holds for all combinations of sides. Use
+    # the strict case to avoid degenerate
     # triangles.
-    ineq12 = s1 + s2 > s3
-    ineq13 = s1 + s3 > s2
-    ineq23 = s2 + s3 > s1
+    ineq12 = s_1 + s_2 > s_3
+    ineq13 = s_1 + s_3 > s_2
+    ineq23 = s_2 + s_3 > s_1
     if not (ineq12 and ineq13 and ineq23):
         return 'invalid'
     # Then, see if it is an equilateral triangle, which can't also be right.
-    if s1 == s2 and s1 == s3:
+    if s_1 == s_2 and s_1 == s_3:
         return 'equilateral'
     # Next, isosceles and scalene triangles can be right, so check that. Watch for floating point.
     # Since the order of the sides doesn't matter, check all possible combinations
-    right12 = s1**2 + s2**2 == s3**2
-    right13 = s1**2 + s3**2 == s2**2
-    right23 = s2**2 + s3**2 == s1**2
+    right12 = s_1**2 + s_2**2 == s_3**2
+    right13 = s_1**2 + s_3**2 == s_2**2
+    right23 = s_2**2 + s_3**2 == s_1**2
     if right12 or right13 or right23:
         result = 'right '
     else:
         result = ''
     # Now see if it is an isosceles triangle
-    iso12 = s1 == s2
-    iso13 = s1 == s3
-    iso23 = s2 == s3
+    iso12 = s_1 == s_2
+    iso13 = s_1 == s_3
+    iso23 = s_2 == s_3
     if iso12 or iso13 or iso23:
         result += 'isosceles'
     else:
@@ -71,5 +78,5 @@ if __name__ == '__main__':
     parser.add_argument('side_3', metavar='side_3',
                         type=float, help='The third side of the triangle.')
     args = parser.parse_args()
-    result = classify_triangle(args.side_1, args.side_2, args.side_3)
-    print('This is a {0:s} triangle.'.format(result))
+    TRIANGLE_TYPE = classify_triangle(args.side_1, args.side_2, args.side_3)
+    print(f'This is a {TRIANGLE_TYPE} triangle.')
